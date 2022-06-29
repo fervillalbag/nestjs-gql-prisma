40f8c83caeeee875bfea914ff338394d91998671
fervillalbag <21fervillalbag@gmail.com>
2022-06-29 12:19:10 -0400

    feat: schema initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
@@ -11,7 +14,10 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
+      driver: ApolloDriver,
+      resolvers: { DateTime: GraphQLDateTime },
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
